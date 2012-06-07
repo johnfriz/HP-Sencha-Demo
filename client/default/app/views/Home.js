7415efd5d1e3cd6bd899544e0885866385e4a005
johnfriz+demo@gmail.com <johnfriz+demo@gmail.com>
2012-06-07 09:28:42 +0000

    update file /client/default/app/views/Home.js

--- client/default/app/views/Home.js
+++ client/default/app/views/Home.js
@@ -7,7 +7,7 @@ app.views.Home = Ext.extend(Ext.Panel, {
   		dock: 'top',
   		xtype: 'toolbar',
   		title: '<img class="logo" src="app/images/logo.png" />',
-      iconCls: 'whiteToolbar'
+      ui: 'light'
   	}
   ],
 
